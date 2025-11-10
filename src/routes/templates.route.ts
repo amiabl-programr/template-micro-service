190fb41166575c881b02600c2074483be43e9748
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 17:23:51 +0100

    refactor: remove unused template routes and retain only the get_templates endpoint

--- src/routes/templates.route.ts
+++ src/routes/templates.route.ts
@@ -2,7 +2,5 @@ import type { FastifyInstance } from "fastify";
 import * as TemplateController from "../controllers/template.controller.ts";
 
 export default async function templateRoutes(app: FastifyInstance) {
-  app.post('/:template_id/render', TemplateController.render_template);
-  app.get('/:template_id', TemplateController.get_template);
-  app.post('/', TemplateController.create_template);
+  app.get('/templates', TemplateController.get_templates);
 }

b08520cc22a0b82a73aaa0db6eb28704377c9869
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 17:30:37 +0100

    feat: add POST endpoint for creating templates

--- src/routes/templates.route.ts
+++ src/routes/templates.route.ts
@@ -3,4 +3,5 @@ import * as TemplateController from "../controllers/template.controller.ts";
 
 export default async function templateRoutes(app: FastifyInstance) {
   app.get('/templates', TemplateController.get_templates);
+  app.post('/templates', TemplateController.create_template);
 }

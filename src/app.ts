8fa2be6c43ac35940b8d11b8c278e189a5f89df1
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 10:40:03 +0100

    feat: register health and templates routes in the Fastify app

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,18 @@ import Fastify from 'fastify';
 import type {FastifyInstance} from 'fastify';
 // import logger from "./config/logger.ts";
 import prisma from './plugins/prisma.ts';
+import healthRoute from './routes/health.route.ts';
+import templatesRoute from './routes/templates.route.ts';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const fastify_app = (): FastifyInstance => {
     const app = Fastify({logger: true})
 
     // register plugins
     app.register(prisma);
+    app.register(healthRoute, { prefix: '/health' });
+    app.register(templatesRoute, { prefix: '/templates' });
 
     // register routes
 

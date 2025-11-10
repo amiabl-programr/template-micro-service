48c7288f37588ba2c50161ceafda80fb2e7aab6f
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 23:40:39 +0100

    feat: add get_template_by_id and update_template endpoints to templates route

--- src/routes/templates.route.ts
+++ src/routes/templates.route.ts
@@ -3,5 +3,7 @@ import * as TemplateController from "../controllers/template.controller.ts";
 
 export default async function templateRoutes(app: FastifyInstance) {
   app.get('/templates', TemplateController.get_templates);
+  app.get('/templates/:id', TemplateController.get_template_by_id);
   app.post('/templates', TemplateController.create_template);
+  app.patch('/templates/:id', TemplateController.update_template);
 }

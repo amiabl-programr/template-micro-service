9a9b37aa298d5db39a07e8dcc5ba739b28758fd4
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 17:23:41 +0100

    refactor: update health route to use healthCheck controller and change endpoint to '/health'

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -1,8 +1,6 @@
 import type { FastifyInstance } from 'fastify';
+import { healthCheck } from '../controllers/health.controller.ts';
 
-export default async function healthRoute(fastify: FastifyInstance) {
-  fastify.get('/', async () => ({
-    success: true,
-    message: 'Template Service healthy'
-  }));
+export default async function healthRoutes(app: FastifyInstance) {
+  app.get('/health', healthCheck);
 }

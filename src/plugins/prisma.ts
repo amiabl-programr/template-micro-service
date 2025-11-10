dcf7e3d5bd9f9622ef49b3d68e81d215851f77b8
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 10:40:25 +0100

    fix: update PrismaClient import path to use generated client

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,5 +1,5 @@
 import fp from 'fastify-plugin'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client.ts';
 
 export default fp(async (fastify) => {
   const prisma = new PrismaClient()

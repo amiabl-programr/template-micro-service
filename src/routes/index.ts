161f45eed37da51400858edc185dde610a3b7922
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-10 07:46:57 +0100

    feat: implement routing for health and template endpoints

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import  type { FastifyInstance } from "fastify";
+import templateRoutes from "./templates.route.ts";
+import healthRoute from "./health.route.ts";
+
+export default async function routes(app: FastifyInstance) {
+    app.register(healthRoute, { prefix: '/health' });
+    app.register(templateRoutes, { prefix: '/templates' });
+}

e16788f656ad8a17032259709a231a842b065e9b
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 10:37:53 +0100

    feat: integrate Swagger for API documentation and improve code formatting

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,50 @@
-import Fastify from 'fastify';
-import type {FastifyInstance} from 'fastify';
+import Fastify from "fastify";
+import type { FastifyInstance } from "fastify";
 // import logger from "./config/logger.ts";
-import prisma from './plugins/prisma.ts';
-import healthRoute from './routes/health.route.ts';
-import templatesRoute from './routes/templates.route.ts';
-import dotenv from 'dotenv';
+import prisma from "./plugins/prisma.ts";
+import healthRoute from "./routes/health.route.ts";
+import templatesRoute from "./routes/templates.route.ts";
+import dotenv from "dotenv";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 dotenv.config();
 
 export const fastify_app = (): FastifyInstance => {
-    const app = Fastify({logger: true})
+  const app = Fastify({ logger: true });
 
-    // register plugins
-    app.register(prisma);
-    app.register(healthRoute);
-    app.register(templatesRoute);
+  // register swagger
+  app.register(swagger, {
+    openapi: {
+      openapi: "3.0.0",
+      info: {
+        title: "My API",
+        description: "API documentation",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+          description: "Development server",
+        },
+      ],
+      tags: [{ name: "templates", description: "Email templates endpoint" }],
+    },
+  });
 
-    // register routes
+  app.register(swaggerUI, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "list",
+      deepLinking: false,
+    },
+    staticCSP: true,
+    transformStaticCSP: (header: string) => header,
+  });
 
-    return app;
+  // register plugins
+  app.register(prisma);
+  app.register(healthRoute);
+  app.register(templatesRoute);
 
-}
+  return app;
+};

86d17906b2a9b2cc62c58fe7b65b9b6722025bb4
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 10:39:01 +0100

    style: standardize quotes and improve formatting in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import  type { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import templateRoutes from "./templates.route.ts";
 import healthRoute from "./health.route.ts";
 
 export default async function routes(app: FastifyInstance) {
-    app.register(healthRoute, { prefix: '/health' });
-    app.register(templateRoutes, { prefix: '/templates' });
+  app.register(healthRoute, { prefix: "/health" });
+  app.register(templateRoutes, { prefix: "/templates" });
 }

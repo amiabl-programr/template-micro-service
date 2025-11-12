78172906f001afee650e1843dcfdb8777dbaf4a9
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 14:42:17 +0100

    style: improve formatting

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const start = async () => {
 
   try {
     await app.listen({ port: 3000, host: "0.0.0.0" });
-    app.log.info(`Server listening at http://localhost:3000`);
+    app.log.info("Server listening at http://localhost:3000");
   } catch (err) {
     app.log.error(err);
     process.exit(1);

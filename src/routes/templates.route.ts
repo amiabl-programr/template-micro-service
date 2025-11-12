d322213dadc327aedc39b2b09afef50bca55197f
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 10:40:32 +0100

    style: standardize quotes and improve formatting in templates.routes.ts

--- src/routes/templates.route.ts
+++ src/routes/templates.route.ts
@@ -2,8 +2,19 @@ import type { FastifyInstance } from "fastify";
 import * as TemplateController from "../controllers/template.controller.ts";
 
 export default async function templateRoutes(app: FastifyInstance) {
-  app.get('/templates', TemplateController.get_templates);
-  app.get('/templates/:id', TemplateController.get_template_by_id);
-  app.post('/templates', TemplateController.create_template);
-  app.patch('/templates/:id', TemplateController.update_template);
+  app.get("/templates", {
+    schema: {
+      description: 'Get all templates',
+      tags: ['templates'],
+      response: {
+        200: {
+          description: 'List of templates',
+          type: 'object'
+        }
+      }
+    }
+  }, TemplateController.get_templates);
+  app.get("/templates/:id", TemplateController.get_template_by_id);
+  app.post("/templates", TemplateController.create_template);
+  app.patch("/templates/:id", TemplateController.update_template);
 }

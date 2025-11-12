cdf5ffc6d05facad01ffeb931e2c4177ee9c3743
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 14:41:32 +0100

    style: improve formatting

--- src/controllers/home.contollers.ts
+++ src/controllers/home.contollers.ts
@@ -1,6 +1,8 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 
-
 export async function homeController(req: FastifyRequest, reply: FastifyReply) {
-   return reply.send({  success: true, message: "Welcome to the Template Service Microservice!" });
+  return reply.send({
+    success: true,
+    message: "Welcome to the Template Service Microservice!",
+  });
 }

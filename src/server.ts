f4c5ca3450a7575218adcd333623383f3ded2054
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-11-12 10:38:08 +0100

    style: improve code formatting in server.ts for consistency

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import {fastify_app} from './app.ts';
+import { fastify_app } from "./app.ts";
 
 const start = async () => {
-    const app = fastify_app();
+  const app = fastify_app();
 
-    try {
-        await app.listen({port: 3000, host: '0.0.0.0'});
-        app.log.info(`Server listening at http://localhost:3000`);
-    } catch (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-}
+  try {
+    await app.listen({ port: 3000, host: "0.0.0.0" });
+    app.log.info(`Server listening at http://localhost:3000`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
 
 start();
